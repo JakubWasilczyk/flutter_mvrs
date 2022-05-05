31c229c6614693c5a24cf47c9a89b815949cca10
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-05 22:41:38 +0200

    changed builder type

--- flutter_mvrs/lib/builder.dart
+++ flutter_mvrs/lib/builder.dart
@@ -5,4 +5,4 @@ import 'package:source_gen/source_gen.dart';
 
 import 'generator/model_generator.dart';
 
-Builder modelBuilder(BuilderOptions options) => SharedPartBuilder([ModelGenerator()], 'model_generator');
+Builder modelBuilder(BuilderOptions options) => PartBuilder([ModelGenerator()], 'base');
