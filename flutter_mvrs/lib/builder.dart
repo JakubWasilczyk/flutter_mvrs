537f15c59436e7628d5fd110c6950a7149a91ebe
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-05 21:53:44 +0200

    Try catch

--- flutter_mvrs/lib/builder.dart
+++ flutter_mvrs/lib/builder.dart
@@ -5,4 +5,11 @@ import 'package:source_gen/source_gen.dart';
 
 import 'generator/model_generator.dart';
 
-Builder modelBuilder(BuilderOptions options) => SharedPartBuilder([ModelGenerator()], 'model_generator');
+Builder modelBuilder(BuilderOptions options) {
+  try {
+    return SharedPartBuilder([ModelGenerator()], 'model_generator');
+  } catch (e) {
+    print(e.toString());
+    throw StateError("Error in model builder");
+  }
+}
