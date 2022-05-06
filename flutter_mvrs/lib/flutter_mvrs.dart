c0fc42846db9511d1b5141a0fdf4141c69d4a43b
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-06 12:41:50 +0200

    ¯\_(ツ)_/¯

--- flutter_mvrs/lib/flutter_mvrs.dart
+++ flutter_mvrs/lib/flutter_mvrs.dart
@@ -1,10 +1,10 @@
 library flutter_mvrs;
 
 export 'annotations/model_annotation.dart';
-export 'src/model/base_model.dart';
-export 'src/model/change_tracker.dart';
-export 'src/model/created_at.dart';
-export 'src/model/updated_at.dart';
+export 'src/models/base_model.dart';
+export 'src/models/change_tracker.dart';
+export 'src/models/created_at.dart';
+export 'src/models/updated_at.dart';
 export 'src/repository/listenable_param_repository.dart';
 export 'src/repository/listenable_repository.dart';
 export 'src/repository/model_param_repository.dart';
--- flutter_mvrs/lib/flutter_mvrs_annotations.dart
+++
@@ -1,3 +0,0 @@
-library flutter_mvrs.annotations;
-
-export 'annotations/model_annotation.dart';
