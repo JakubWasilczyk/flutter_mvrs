e82a03a0d29ff1289a2a67579394aa9de312647e
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-06 15:47:41 +0200

    This is not a commit

--- flutter_mvrs/lib/builder.dart
+++ flutter_mvrs/lib/builder.dart
@@ -10,7 +10,7 @@ Builder modelBuilder(BuilderOptions options) => PartBuilder(
       '.base.dart',
       options: const BuilderOptions({
         "build_extensions": {
-          "{{}}.dart": "base/{{}}.base.dart",
+          "{{dir}}/{{file}}.dart": "{{dir}}/base/{{file}}.base.dart",
         },
       }),
     );
