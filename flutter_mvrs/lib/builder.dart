a6370b92b693b53a54e5576b59bbf59177829cb5
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-06 15:36:11 +0200

    lol digg

--- flutter_mvrs/lib/builder.dart
+++ flutter_mvrs/lib/builder.dart
@@ -5,4 +5,12 @@ import 'package:source_gen/source_gen.dart';
 
 import 'generator/model_generator.dart';
 
-Builder modelBuilder(BuilderOptions options) => PartBuilder([ModelGenerator()], '.base.dart');
+Builder modelBuilder(BuilderOptions options) => PartBuilder(
+      [ModelGenerator()],
+      '.base.dart',
+      options: const BuilderOptions({
+        "build_extensions": {
+          "{{dir}}/{{file}}.dart": ["{{dir}}/base/{{file}}.base.dart"],
+        },
+      }),
+    );
