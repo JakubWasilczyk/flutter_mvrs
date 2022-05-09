c6cb843faebe8b3416b248b50c7461273dc4ea5b
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-09 12:45:59 +0200

    Fix in model param repository

--- flutter_mvrs/lib/src/repository/model_param_repository.dart
+++ flutter_mvrs/lib/src/repository/model_param_repository.dart
@@ -1,6 +1,6 @@
 abstract class ModelParamRepository<T, P> {
   Future<T?> get(String id, P params);
-  Future<T?> save(T model, P params);
+  Future<String> save(T model, P params);
   Future<List<T>> getList(P params);
   Future<void> delete(T model, P params);
 }
