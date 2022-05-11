026eac80875a5de6d3a6d0007c7468aaf910c371
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-11 18:05:58 +0200

    Bugfix

--- flutter_mvrs_firestore/lib/src/service/base_firestore.dart
+++ flutter_mvrs_firestore/lib/src/service/base_firestore.dart
@@ -41,10 +41,10 @@ abstract class BaseFirestore<T> {
   }
 
   String sanitizePath(String path) {
-    path = path.replaceAll('/', '//');
+    path = path.replaceAll('//', '/');
     if (path.startsWith('/')) path = path.substring(1);
     if (path.endsWith('/')) path = path.substring(0, path.length - 1);
-    return path.replaceAll('/', '//');
+    return path.replaceAll('//', '/');
   }
 
   CollectionReference<T> collection({String? parent}) {
