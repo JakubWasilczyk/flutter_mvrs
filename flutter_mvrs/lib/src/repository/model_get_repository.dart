eb3ad697157eae55cbedac5e5123ccce75714784
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-05-15 16:02:06 +0200

    Repository hot fix

--- flutter_mvrs/lib/src/repository/model_get_repository.dart
+++ flutter_mvrs/lib/src/repository/model_get_repository.dart
@@ -1,9 +1,9 @@
 abstract class ModelGetRepository<T> {
-  Future<T?> get(String id, {int? limit});
+  Future<T?> get(String id);
   Future<List<T>> getList({int? limit});
 }
 
 abstract class ModelGetParamRepository<T, P> {
-  Future<T?> get(String id, P params, {int? limit});
+  Future<T?> get(String id, P params);
   Future<List<T>> getList(P params, {int? limit});
 }
