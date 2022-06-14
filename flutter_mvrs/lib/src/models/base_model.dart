7cb2b92c8a47676096f665efb348a488d3e0170d
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-06-14 09:16:35 +0200

    Hotfix on BaseModel

--- flutter_mvrs/lib/src/models/base_model.dart
+++ flutter_mvrs/lib/src/models/base_model.dart
@@ -3,7 +3,10 @@ import 'package:equatable/equatable.dart';
 import 'change_tracker.dart';
 
 abstract class BaseModel<T> extends Equatable with ChangeTracker {
-  final T id;
+  final T _id;
 
-  BaseModel({required this.id});
+  BaseModel({required T id}) : _id = id;
+
+  T get id => get("id", _id);
+  set id(T value) => set("id", value);
 }
