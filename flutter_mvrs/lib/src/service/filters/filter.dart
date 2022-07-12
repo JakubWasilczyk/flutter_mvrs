b95f264bf633a8c9061d9f65c5644e0ee7dc346b
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-07-12 16:41:28 +0200

    Removed back filters apply function

--- flutter_mvrs/lib/src/service/filters/filter.dart
+++ flutter_mvrs/lib/src/service/filters/filter.dart
@@ -1,10 +1,20 @@
-abstract class Filter {
-  dynamic apply(dynamic query);
-}
+abstract class Filter {}
 
 abstract class Where implements Filter {
   final Object field;
   final dynamic value;
 
   Where(this.field, {this.value});
+}
+
+enum OrderByDirection { ascending, descending }
+
+abstract class OrderBy implements Filter {
+  final Object field;
+  final OrderByDirection direction;
+
+  OrderBy(this.field, this.direction);
+
+  bool get isDescending => direction == OrderByDirection.descending;
+  bool get isAscending => direction == OrderByDirection.ascending;
 }
