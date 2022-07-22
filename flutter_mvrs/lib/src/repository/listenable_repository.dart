a6dc8fbda10683f32f19242ad7c7824d68c4271e
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-07-22 12:02:11 +0200

    Removed unused params

--- flutter_mvrs/lib/src/repository/listenable_repository.dart
+++ flutter_mvrs/lib/src/repository/listenable_repository.dart
@@ -1,9 +1,9 @@
 abstract class ListenableRepository<T> {
   Stream<T?> listen(String id);
-  Stream<List<T>> listenList({int? limit});
+  Stream<List<T>> listenList();
 }
 
 abstract class ListenableParamRepository<T, P> {
   Stream<T?> listen(String id, P param);
-  Stream<List<T>> listenList(P param, {int? limit});
+  Stream<List<T>> listenList(P param);
 }
--- flutter_mvrs/lib/src/repository/model_get_repository.dart
+++ flutter_mvrs/lib/src/repository/model_get_repository.dart
@@ -1,9 +1,9 @@
 abstract class ModelGetRepository<T> {
   Future<T?> get(String id);
-  Future<List<T>> getList({int? limit});
+  Future<List<T>> getList();
 }
 
 abstract class ModelGetParamRepository<T, P> {
   Future<T?> get(String id, P param);
-  Future<List<T>> getList(P param, {int? limit});
+  Future<List<T>> getList(P param);
 }
