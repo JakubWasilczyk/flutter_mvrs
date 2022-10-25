f8963a54ad5c9b5d73a08126f6b57c5cbfe7d425
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-10-25 22:27:02 +0200

    Revert "Fixed model listen in firebase"
    This reverts commit 9f67eb4537b09c75304e8f0d4a549f0fbe24ccb8.

--- flutter_mvrs_firestore/lib/src/repository/listenable_repository.dart
+++ flutter_mvrs_firestore/lib/src/repository/listenable_repository.dart
@@ -2,7 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_mvrs/flutter_mvrs.dart';
 import 'package:flutter_mvrs_firestore/flutter_mvrs_firestore.dart';
 
-mixin ModelListenServiceRepository<T extends BaseModel> on ServiceRepository<T> {
+abstract class ListenableServiceRepository<T extends BaseModel> extends ListenableRepository<T> {
+  final CollectionProvider<T> service;
+
+  ListenableServiceRepository(this.service);
+
+  @override
   Stream<DataState<T?>> listen(String id) {
     try {
       return service.listen(id).map((r) => DataSuccess(r));
@@ -13,6 +18,7 @@ mixin ModelListenServiceRepository<T extends BaseModel> on ServiceRepository<T> 
     }
   }
 
+  @override
   Stream<DataState<List<T>>> listenList() {
     try {
       return service.listenList().map((r) => DataSuccess(r));
@@ -24,7 +30,12 @@ mixin ModelListenServiceRepository<T extends BaseModel> on ServiceRepository<T> 
   }
 }
 
-mixin ModelListenParamServiceRepository<T extends BaseModel, P> on ServiceParamRepository<T, P> {
+abstract class ListenableParamServiceRepository<T extends BaseModel, P> extends ListenableParamRepository<T, P> {
+  final SubCollectionProvider<T, P> service;
+
+  ListenableParamServiceRepository(this.service);
+
+  @override
   Stream<DataState<T?>> listen(String id, P param) {
     try {
       return service.listen(param, id).map((r) => DataSuccess(r));
@@ -35,6 +46,7 @@ mixin ModelListenParamServiceRepository<T extends BaseModel, P> on ServiceParamR
     }
   }
 
+  @override
   Stream<DataState<List<T>>> listenList(P param) {
     try {
       return service.listenList(param).map((r) => DataSuccess(r));
--- flutter_mvrs_firestore/lib/src/repository/model_repository.dart
+++ flutter_mvrs_firestore/lib/src/repository/model_repository.dart
@@ -2,11 +2,11 @@ import 'package:flutter_mvrs/flutter_mvrs.dart';
 import 'package:flutter_mvrs_firestore/flutter_mvrs_firestore.dart';
 
 abstract class ModelServiceRepository<T extends BaseModel> extends ServiceRepository<T>
-    with ModelGetServiceRepository, ModelSetServiceRepository, ModelListenServiceRepository {
+    with ModelGetServiceRepository, ModelSetServiceRepository {
   ModelServiceRepository(CollectionProvider<T> service) : super(service);
 }
 
 abstract class ModelParamServiceRepository<T extends BaseModel, P> extends ServiceParamRepository<T, P>
-    with ModelGetParamServiceRepository, ModelSetParamServiceRepository, ModelListenParamServiceRepository {
+    with ModelGetParamServiceRepository, ModelSetParamServiceRepository {
   ModelParamServiceRepository(SubCollectionProvider<T, P> service) : super(service);
 }
